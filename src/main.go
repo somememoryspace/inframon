f4e80dd7c8e0b577c692d73c4616c94a3815c94b
ali <87505602+somememoryspace@users.noreply.github.com>
2024-07-29 02:12:57 -0400

    More corrections

--- src/main.go
+++ src/main.go
@@ -81,7 +81,7 @@ func pingTaskICMP(ip string, hostname string, tags []string, retryBuffer int, ti
 				if consecutiveFailures > retryBuffer {
 					utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("icmp ::::: could not connect to address: (%s)", ip), "error", STDOUT)
 					setICMPHealth(ip, false)
-					sendToDiscordWebhookFailure(discordWebhookURL, hostname, "N/A", tags, "ICMP [BROKEN]")
+					sendToDiscordWebhookFailure(discordWebhookURL, hostname, "N/A", tags, "ICMP [BROKEN]", true)
 				}
 				time.Sleep(1 * time.Second)
 				continue
@@ -89,6 +89,7 @@ func pingTaskICMP(ip string, hostname string, tags []string, retryBuffer int, ti
 		} else {
 			if !getICMPHealth(ip) {
 				setICMPHealth(ip, true)
+				sendToDiscordWebhookFailure(discordWebhookURL, hostname, "N/A", tags, "ICMP SUCCESS", false)
 			}
 			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("icmp ::::: %s (%s) (%s) ::::: latency %v", hostname, ip, utils.FormatTags(tags), latency), "info", STDOUT)
 			time.Sleep(time.Duration(timeout) * time.Second)
@@ -98,115 +99,164 @@ func pingTaskICMP(ip string, hostname string, tags []string, retryBuffer int, ti
 }
 
 func pingHTTP(hostname string, retryBuffer int, skipverify bool, wg *sync.WaitGroup) (int, error) {
-	wg.Add(2)
+	wg.Add(1)
 	defer wg.Done()
 	httpClient := &http.Client{
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: skipverify},
 		},
 	}
-	consecutiveFailures := 0
-	for {
-		if !strings.HasPrefix(hostname, "http://") && !strings.HasPrefix(hostname, "https://") {
-			setHTTPHealth(hostname, false)
-			return 0, nil
-		}
-		resp, err := httpClient.Get(hostname)
-		if err != nil {
-			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("http ::::: could not connect to address: (%s)", hostname), "error", STDOUT)
-			setHTTPHealth(hostname, false)
-			consecutiveFailures++
-			if consecutiveFailures > retryBuffer {
-				return 0, err
-			}
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		defer resp.Body.Close()
-		if resp.StatusCode != 200 && resp.StatusCode != 201 && resp.StatusCode != 204 {
-			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("http ::::: received non-success status code (%d) for (%s)", resp.StatusCode, hostname), "error", STDOUT)
-			setHTTPHealth(hostname, false)
-			consecutiveFailures++
-			if consecutiveFailures > retryBuffer {
-				return resp.StatusCode, fmt.Errorf("http error: %s, ::::: [%s]", hostname, resp.Status)
-			}
-			time.Sleep(1 * time.Second)
-			continue
-		}
-		setHTTPHealth(hostname, true)
-		consecutiveFailures = 0
+	if !strings.HasPrefix(hostname, "http://") && !strings.HasPrefix(hostname, "https://") {
+		return 0, nil
+	}
+	resp, err := httpClient.Get(hostname)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 && resp.StatusCode != 201 && resp.StatusCode != 204 {
+		utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("http ::::: received non-success status code (%d) for (%s)", resp.StatusCode, hostname), "error", STDOUT)
 		return resp.StatusCode, nil
 	}
+	return resp.StatusCode, nil
 }
 
 func pingTaskHTTP(fqdn string, tags []string, timeout int, wg *sync.WaitGroup, discordWebhookURL string, retrybuffer int, skipverify bool) {
+	wg.Add(1)
 	defer wg.Done()
+	consecutiveFailures := 0
 	for {
 		respCode, err := pingHTTP(fqdn, retrybuffer, skipverify, wg)
-		if err != nil && respCode != 0 {
-			sendToDiscordWebhookFailure(discordWebhookURL, "N/A", fqdn, tags, fmt.Sprintf("HTTP [%v]", respCode))
+		if err != nil || respCode == 0 {
+			if getHTTPHealth(fqdn) {
+				consecutiveFailures++
+				if consecutiveFailures > retrybuffer {
+					utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("http ::::: could not connect to host: (%s)", fqdn), "error", STDOUT)
+					setHTTPHealth(fqdn, false)
+					sendToDiscordWebhookFailure(discordWebhookURL, fqdn, "N/A", tags, "HTTP [BROKEN]", true)
+				}
+				time.Sleep(1 * time.Second)
+				continue
+			}
+		} else if respCode == 200 || respCode == 201 || respCode == 204 {
+			if !getHTTPHealth(fqdn) {
+				setICMPHealth(fqdn, true)
+				sendToDiscordWebhookFailure(discordWebhookURL, fqdn, "N/A", tags, "HTTP SUCCESS", false)
+			}
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("http ::::: %s (%s) ::: [%v]", fqdn, utils.FormatTags(tags), respCode), "info", STDOUT)
+			time.Sleep(time.Duration(timeout) * time.Second)
 		}
-		if respCode == 0 {
-			sendToDiscordWebhookFailure(discordWebhookURL, "N/A", fqdn, tags, "HTTP [UNREACHABLE]")
-		}
-		utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("http ::::: %s (%s) ::: [%v]", fqdn, utils.FormatTags(tags), respCode), "info", STDOUT)
-		time.Sleep(time.Duration(timeout) * time.Second)
+		consecutiveFailures = 0
 	}
 }
 
-func sendToDiscordWebhookFailure(webhookURL string, hostname string, fqdn string, tags []string, errorMessage string) {
-	embed := utils.DiscordEmbed{
-		Title:       "Unable to Connect",
-		Description: errorMessage,
-		Color:       0xFF0000,
-		Fields: []utils.DiscordField{
-			{
-				Name:   "Hostname",
-				Value:  hostname,
-				Inline: true,
+func sendToDiscordWebhookFailure(webhookURL string, hostname string, fqdn string, tags []string, messageBody string, typeFailure bool) {
+	if typeFailure {
+		embed := utils.DiscordEmbed{
+			Title:       "Unable to Connect",
+			Description: messageBody,
+			Color:       0xFF0000,
+			Fields: []utils.DiscordField{
+				{
+					Name:   "Hostname",
+					Value:  hostname,
+					Inline: true,
+				},
+				{
+					Name:   "FQDN",
+					Value:  fqdn,
+					Inline: true,
+				},
+				{
+					Name:   "Date",
+					Value:  time.Now().Format("2006-01-02"),
+					Inline: true,
+				},
+				{
+					Name:   "Time",
+					Value:  time.Now().Format("15:04:05"),
+					Inline: true,
+				},
+				{
+					Name:   "Tags",
+					Value:  string(utils.FormatTags(tags)),
+					Inline: true,
+				},
 			},
-			{
-				Name:   "FQDN",
-				Value:  fqdn,
-				Inline: true,
+		}
+		message := utils.Message{
+			Embeds: []utils.DiscordEmbed{embed},
+		}
+		jsonPayload, err := json.Marshal(message)
+		if err != nil {
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: error marshalling json: %v", err), "error", STDOUT)
+			return
+		}
+		resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
+		if err != nil {
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: error sending Discord webhook: %v", err), "error", STDOUT)
+			return
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 204 {
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: unexpected status code: [%d]", resp.StatusCode), "error", STDOUT)
+			return
+		}
+		utils.ConsoleAndLoggerOutput(LOGGER, "syst ::::: message sent successfully to discord webhook", "info", STDOUT)
+	} else {
+		embed := utils.DiscordEmbed{
+			Title:       "Connecion Success",
+			Description: messageBody,
+			Color:       0x00FF00,
+			Fields: []utils.DiscordField{
+				{
+					Name:   "Hostname",
+					Value:  hostname,
+					Inline: true,
+				},
+				{
+					Name:   "FQDN",
+					Value:  fqdn,
+					Inline: true,
+				},
+				{
+					Name:   "Date",
+					Value:  time.Now().Format("2006-01-02"),
+					Inline: true,
+				},
+				{
+					Name:   "Time",
+					Value:  time.Now().Format("15:04:05"),
+					Inline: true,
+				},
+				{
+					Name:   "Tags",
+					Value:  string(utils.FormatTags(tags)),
+					Inline: true,
+				},
 			},
-			{
-				Name:   "Date",
-				Value:  time.Now().Format("2006-01-02"),
-				Inline: true,
-			},
-			{
-				Name:   "Time",
-				Value:  time.Now().Format("15:04:05"),
-				Inline: true,
-			},
-			{
-				Name:   "Tags",
-				Value:  string(utils.FormatTags(tags)),
-				Inline: true,
-			},
-		},
+		}
+		message := utils.Message{
+			Embeds: []utils.DiscordEmbed{embed},
+		}
+		jsonPayload, err := json.Marshal(message)
+		if err != nil {
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: error marshalling json: %v", err), "error", STDOUT)
+			return
+		}
+		resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
+		if err != nil {
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: error sending Discord webhook: %v", err), "error", STDOUT)
+			return
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 204 {
+			utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: unexpected status code: [%d]", resp.StatusCode), "error", STDOUT)
+			return
+		}
+		utils.ConsoleAndLoggerOutput(LOGGER, "syst ::::: message sent successfully to discord webhook", "info", STDOUT)
 	}
-	message := utils.Message{
-		Embeds: []utils.DiscordEmbed{embed},
-	}
-	jsonPayload, err := json.Marshal(message)
-	if err != nil {
-		utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: error marshalling json: %v", err), "error", STDOUT)
-		return
-	}
-	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonPayload))
-	if err != nil {
-		utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: error sending Discord webhook: %v", err), "error", STDOUT)
-		return
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 204 {
-		utils.ConsoleAndLoggerOutput(LOGGER, fmt.Sprintf("syst ::::: unexpected status code: [%d]", resp.StatusCode), "error", STDOUT)
-		return
-	}
-	utils.ConsoleAndLoggerOutput(LOGGER, "syst ::::: message sent successfully to discord webhook", "info", STDOUT)
+
 }
 
 func healthCheck(timeout int, wg *sync.WaitGroup) {
