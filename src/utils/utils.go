0f8705ce6c409303789e4acc82ee42f8917ca81d
ali <87505602+somememoryspace@users.noreply.github.com>
2024-08-03 03:52:16 -0400

    Updates to stdout

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -105,12 +105,12 @@ func CreateLogEntry(Type string, Message string, Event string) (string, error) {
 }
 
 func ValidateLogDirectory(directoryPath string) {
-	fmt.Printf("message: system ::::: runtime[LOG] :: validating %s directory available... skipping if available\n", directoryPath)
+	fmt.Printf("message: system :: runtime[LOG] :: validating %s directory available... skipping if available\n", directoryPath)
 	err := os.MkdirAll(directoryPath, os.ModePerm)
 	if err != nil {
-		panic(fmt.Sprintf("message: system ::::: runtime[LOG] :: unable to create directory: %s", directoryPath))
+		panic(fmt.Sprintf("message: system :: runtime[LOG] :: unable to create directory: %s", directoryPath))
 	}
-	fmt.Printf("message: system ::::: runtime[LOG] :: validated %s directory available\n", directoryPath)
+	fmt.Printf("message: system :: runtime[LOG] :: validated %s directory available\n", directoryPath)
 }
 
 func SetupLogger(directoryPath string, logName string) *log.Logger {
@@ -118,7 +118,7 @@ func SetupLogger(directoryPath string, logName string) *log.Logger {
 	logFilePath := fmt.Sprintf("%s/runtime.log", directoryPath)
 	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		panic(fmt.Sprintf("message: system ::::: runtime[LOG] :: unable to open log file: %s", logFilePath))
+		panic(fmt.Sprintf("message: system :: runtime[LOG] :: unable to open log file: %s", logFilePath))
 	}
 	logger := log.New(file, "event: ", log.Ldate|log.Ltime|log.Lshortfile)
 	return logger
@@ -129,12 +129,12 @@ func ConsoleAndLoggerOutput(logger *log.Logger, Type string, Message string, Eve
 	formattedTime := currentTime.Format("2006-01-02 15:04:05")
 	logEntry, err := CreateLogEntry(Type, Message, Event)
 	if err != nil {
-		fmt.Printf("system ::::: runtime[LOG] :: error creating log entry: %v\n", err)
+		fmt.Printf("system :: runtime[LOG] :: error creating log entry: %v\n", err)
 		return
 	}
 	logger.Println(logEntry)
 	if ConsoleOut {
-		fmt.Printf("%s ::::: message: %s ::::: %s ::::: event: %s \n", formattedTime, Type, Message, Event)
+		fmt.Printf("%s ::::: message: %s :: %s ::::: event: %s \n", formattedTime, Type, Message, Event)
 	}
 }
 
